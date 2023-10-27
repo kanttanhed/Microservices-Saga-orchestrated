2bf0f32aa542fdac2a1b84208943cf5dd8798100
Plinio <plinio.salazar@yahoo.com.br>
2023-10-27 16:55:55 -0300

    Initial project

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/OrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.orchestrated.productvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationServiceApplication.class, args);
+	}
+
+}
