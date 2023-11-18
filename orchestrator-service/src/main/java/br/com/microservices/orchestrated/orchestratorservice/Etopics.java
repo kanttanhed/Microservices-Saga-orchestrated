f6ba21c04dae61ed021021d3efd0092209c9aadd
Plinio <plinio.salazar@yahoo.com.br>
2023-11-18 11:28:45 +0000

    Feat: Mapping and define the Orchestrator Topic

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/Etopics.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Etopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+
+}
