7610cef253c806316b5f69713de60ac6fc5ce0b2
Plinio <plinio.salazar@yahoo.com.br>
2023-12-05 10:11:51 +0000

    Feat: Creating produces

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/producer/kafkaProducer.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.inventoryservice.core.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class kafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            log.info("Sending event to topic {} with data {}", orchestratorTopic, payload);
+            kafkaTemplate.send(orchestratorTopic, payload);
+        } catch (Exception ex) {
+            log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload, ex);
+        }
+    }
+}
