30c212f2c54137e5def43b789ba456bc88fa6f39
Plinio <plinio.salazar@yahoo.com.br>
2023-12-05 09:39:14 +0000

    Feat: Creating Json Util

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/utils/JsonUtil.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.utils;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.dto.EventDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object){
+        try{
+            return objectMapper.writeValueAsString(object);
+        }catch (Exception e){
+            return "";
+        }
+    }
+    
+    public EventDto toEvent(String json){
+        try{
+            return objectMapper.readValue(json, EventDto.class);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/utils/JsonUtil.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.orderservice.core.utils;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object){
+        try{
+            return objectMapper.writeValueAsString(object);
+        }catch (Exception e){
+            return "";
+        }
+    }
+
+    public Event toEvent(String json){
+        try{
+            return objectMapper.readValue(json, Event.class);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
