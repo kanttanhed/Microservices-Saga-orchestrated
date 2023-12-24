4e49f0e0ecc08f4752c98f272ef62ec324d6bb08
Plinio <plinio.salazar@yahoo.com.br>
2023-12-24 12:47:13 +0000

    Feat: Creating the logic of receiving finish Saga notification

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.consumer;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EventConsumer {
 
+    private final EventService eventService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -20,6 +22,6 @@ public class EventConsumer {
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Receiving ending notification event {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        eventService.notifyEnding(event);
     }
 }
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -3,13 +3,24 @@ package br.com.microservices.orchestrated.orderservice.core.service;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventService {
 
         private final EventRepository repository;
+
+        public void notifyEnding(Event event){
+            event.setOrderId(event.getOrderId());
+            event.setCreatedAt(LocalDateTime.now());
+            save(event);
+            log.info("Order {} with Saga notified! TransactionalId: {}", event.getOrderId(),event.getTransactionId());
+        }
 
         public Event save(Event event){
             return repository.save(event);
