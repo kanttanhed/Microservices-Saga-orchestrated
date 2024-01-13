436a458c158079313a83e9ad9e753ebb47f3edff
Plinio <plinio.salazar@yahoo.com.br>
2024-01-13 14:17:48 +0000

    style: Adjust indent

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/Inventory.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.inventoryservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String productCode;
+
+    @Column(nullable = false)
+    private Integer available;
+}
