98cf637aff0a3a6c018aaf8bca11eb0d8744880c
Plinio <plinio.salazar@yahoo.com.br>
2024-01-16 23:32:17 +0000

    feat: Adding entities of Inventory

---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.inventory (id, product_code, available) VALUES (1, 'COMIC_BOOKS', 4);
+INSERT INTO public.inventory (id, product_code, available) VALUES (2, 'BOOKS', 2);
+INSERT INTO public.inventory (id, product_code, available) VALUES (3, 'MOVIES', 5);
+INSERT INTO public.inventory (id, product_code, available) VALUES (4, 'MUSIC', 9);
